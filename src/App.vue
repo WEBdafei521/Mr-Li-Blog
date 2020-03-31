f12501f3db60aed2474c4f87cc7f5d04a34016c3
李鹏 <591094678@qq.com>
2020-03-31 18:06:02 +0800

    个人博客 2020/03/31 18:05

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <vue-particles
+        color="#000"
+        :particleOpacity=".7"
+        :particlesNumber="70"
+        shapeType="circle"
+        :particleSize="1"
+        linesColor="#000000"
+        :linesWidth="1"
+        :lineLinked="true"
+        :lineOpacity="0.4"
+        :linesDistance="150"
+        :moveSpeed="2"
+        :hoverEffect="true"
+        hoverMode="grab"
+        :clickEffect="true"
+        clickMode="push"
+        class="lizi"
+      >
+      </vue-particles>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app{
+}
+body{
+  margin: 0;
+  padding:0;
+}
+.lizi{
+  position: absolute;
+  z-index: -1;
+  width: 100%;
+  height: 99%;
+}
+</style>
